313cf4bcda418a01d3aa57ecc09854ae21585c26
lucas.silva <lucas.silva@jazztech.com.br>
2025-05-26 23:32:46 -0300

    hello world e teste unitario

---
+++ atividade/src/main/java/com/example/atividade/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.atividade;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
